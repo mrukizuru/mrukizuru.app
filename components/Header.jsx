a9e6b8d54fdcb7420ced8c7cccb6d1c63caec53c
mrukizuru <mr.ukizuru@gmail.com>
2024-08-14 12:17:47 -0600

    Update next.config.mjs

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,5 +1,4 @@
 import ThemeToggler  from './ThemeToggler';
-
 const Header = () => {
   return (
     <header>
--- components/ThemeToggler.jsx
+++ components/ThemeToggler.jsx
@@ -7,11 +7,11 @@ const ThemeToggler = () => {
   const {theme, setTheme} = useTheme();
   return <div>
      <Button> 
-      <SunIcon/>
-      <MoonIcon/>  
+      <SunIcon className='h-[1,2rem] w-[1.2rem] rotate-0 scale-100 transition-all
+      dark:-rotate-90 dark:scale-0  dark:scale-100'/>
+      <MoonIcon className='absolute h-[1.2rem] w-[1.2] rotate-90 scale-0 transition-all
+       dark:rotate-0 dark:scale-100'/>  
      </Button>
       </div>;
-
 }
-
 export default ThemeToggler
